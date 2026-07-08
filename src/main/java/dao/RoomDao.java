19761fa690b34e1529ae9755d3481db2dab4b290
takeshita-05 <intecs.2603ap.05@gmail.com>
2026-07-08 15:30:35 +0900

    ろーかる

--- src/main/java/dao/RoomDao.java
+++ src/main/java/dao/RoomDao.java
@@ -3,21 +3,40 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 import bean.RoomBean;
 
 public class RoomDao {
+	public static void main(String args[]) {
+		try {
+			RoomDao.findAll();
+		} catch (ClassNotFoundException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			System.out.println("クラスがが見つかりません。");
+		} catch (SQLException e) {
+			// TODO 自動生成された catch ブロック
+			e.printStackTrace();
+			System.out.println("SQLに関するえらーです。");
+			
+			
+		}
+		
+	}
 		//コンストラクタ
 		private RoomDao() {}
 		//メソッド-----すべての会議室を検索する
-		public static RoomBean[] findAll() {
+		public static RoomBean[] findAll() throws SQLException, ClassNotFoundException {
 			//JDBC固定コンボ4つ
 			String sql = "SELECT * FROM room"; 
+		try(
 			Connection conn = ConnectionProvider.getConnection();
-			PreparedStatement pstmt = conn.prepareStatement(sql);
-			ResultSet rs = pstmt.executeQuery();
+			PreparedStatement pstmt = conn.prepareStatement(sql)){
+		try(
+			ResultSet rs = pstmt.executeQuery()){
 			
 			List <RoomBean> list = new ArrayList <RoomBean>();
 			while(rs.next()) {
@@ -33,5 +52,9 @@ public class RoomDao {
 			}
 				return list.toArray(new RoomBean[0]);
 	
-		}}
+		}catch(SQLException e) {
+			throw new SQLException();
+		}
+			
+		}}}
 
