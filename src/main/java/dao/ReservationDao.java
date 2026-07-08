2ca4c1b847939df8ee81f7912d8bbe68572e7934
pompom <intecs.2603ap.06@gmail.com>
2026-07-08 15:28:06 +0900

    ReservationDaoのページを更新しました

---
+++ src/main/java/dao/ReservationDao.java
@@ -0,0 +1,72 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
+import bean.ReservationBean;
+
+public class ReservationDao {
+	//	コンストラクタ
+	private ReservationDao() {
+	}
+
+	//	メソッド
+	//	利用日の予約日を検索
+	public static List<ReservationBean> findByDate(String date) {
+		//DB取得結果を格納するリスト
+		ReservationBean rList = new ReservationBean();
+		//		データベース接続
+		String sql = "SELECT * FROM reservation WHERE date = ?";
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			//					プレースホルダーに値を設定
+			pstmt.setString(1, reservation.getDate());
+			try (ResultSet rs = pstmt.executeQuery()) {
+				while (rs.next()) {
+					String yoyakudate = rs.getString("date");
+					ReservationBean kensaku = new ReservationBean();
+					//rList.add(kensaku)
+				}
+			}
+		}
+
+		return null;
+	}
+
+	//	予約を追加するメソッド
+	public static boolean insert(ReservationBean reservation) throws ClassNotFoundException {
+		String sql = "INSERT INTO reservation(roomId,date,start,end,userId)VALUES(?,?,?,?,?)";
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setString(1, reservation.getRoomId());
+			pstmt.setString(2, reservation.getDate());
+			pstmt.setString(3, reservation.getStart());
+			pstmt.setString(4, reservation.getEnd());
+			pstmt.setString(5, reservation.getUserId());
+			int ret = pstmt.executeUpdate();
+			return ret != 0;
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+
+	//	予約を削除するメソッド
+	public static boolean delete(ReservationBean reservation) throws ClassNotFoundException {
+		String sql = "SELECT * FROM reservation WHERE id = ?";
+		try (Connection conn = ConnectionProvider.getConnection();
+				PreparedStatement pstmt = conn.prepareStatement(sql)) {
+			pstmt.setInt(1, reservation.getId());
+			int ret = -1;
+			//executeQueryメソッド実行する
+			ret = pstmt.executeUpdate();
+			System.out.println("予約を" + ret + "件、キャンセルしました");
+		} catch (SQLException e) {
+			e.printStackTrace();
+		}
+		return false;
+	}
+};
