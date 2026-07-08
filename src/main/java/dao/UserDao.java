10d3f53f4d862bf28543596cd3db36346cad73ec
ikeiko <f75934670@gmail.com>
2026-07-08 11:53:05 +0900

    dao更新した

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -10,12 +10,18 @@ public class UserDao {
 
 	//コンストラクタ------
 	UserDao(){}
-	//メソッド------
+	
+	//利用者IDとパスワードで利用者認証を行い，認証した利用者情報を返します。
 	public static UserBean certificate(String id,String password){
+		if (id == null || id.isEmpty() || password == null || password.isEmpty()) {
+            return null;
+            
 	//DB取得結果を格納する
 	User=new User();
+	
 	//データベース接続
 	String sql="INSERT INTo user(id,password)VALUES(?,?)";
+	
 	// try-with-resources構文でリソースを自動的にクローズ
 	try (
             Connection conn = UserConnectionProvider.getConnection();
@@ -26,6 +32,11 @@ public class UserDao {
         //SQL文を実行して結果を取得
         try(ResultSet rs=pstmt.executeQuery()){
         	//結果セットをViewへ送るための準備
+        	while(rs.next()){
+        		//結果セットから取得
+        		String id=rs.getString("id");
+        		String password=rs.getString("password");
+        	}
         }
 	}
 	}
