960bd2986e27c7ffcfc4ad611c51acdc7ae14bfb
takeshita-05 <intecs.2603ap.05@gmail.com>
2026-07-08 11:52:11 +0900

    げんきです

---
+++ src/main/java/dao/RoomDao.java
@@ -0,0 +1,38 @@
+package dao;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+
+import bean.RoomBean;
+
+public class RoomDao {
+		//コンストラクタ
+		private RoomDao() {}
+		//メソッド-----すべての会議室を検索する
+		public static RoomBean[] findAll() {
+			//JDBC固定コンボ4つ
+			String sql = "SELECT * FROM room"; 
+			Connection conn = ConnectionProvider
+
+			PreparedStatement pstmt = conn.prepareStatement(sql);
+			ResultSet rs = pstmt.executeQuery();
+			
+			List <RoomBean> list = new ArrayList <RoomBean>();
+			while(rs.next()) {
+				RoomBean bean = new RoomBean(rs.getString("id"),rs.getString("name"));
+				list.add(bean);	
+			}
+			if(list.size() == 0) {
+				return null;
+			}
+			RoomBean[] result = new RoomBean[list.size()];
+			for(int i = 0 ; i < list.size(); ++i){
+				result[i] = list.get(i);
+			}
+				return result;
+	
+		}}
+
