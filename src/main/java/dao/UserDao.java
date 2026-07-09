b6a26507272b9175393500280beade5cb30777bd
ikeiko <f75934670@gmail.com>
2026-07-09 10:25:57 +0900

    dao更新した

--- src/main/java/dao/UserDao.java
+++ src/main/java/dao/UserDao.java
@@ -3,24 +3,24 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-
-import bean.UserBean; 
+import java.sql.SQLException; 
 
 public class UserDao {
 
+
 	//コンストラクタ------
-	UserDao(){}
+	private UserDao(){}
 	
 	//利用者IDとパスワードで利用者認証を行い，認証した利用者情報を返します。
-	public static UserBean certificate(String id,String password){
+	public static Object certificate(String id,String password){
 		if (id == null || id.isEmpty() || password == null || password.isEmpty()) {
             return null;
             
 	//DB取得結果を格納する
-	User=new User();
+	String[] userData=new String[3];
 	
 	//データベース接続
-	String sql="INSERT INTo user(id,password)VALUES(?,?)";
+	String sql="SELECT*FROM user WHERE user_id=?";
 	
 	// try-with-resources構文でリソースを自動的にクローズ
 	try (
@@ -34,11 +34,22 @@ public class UserDao {
         	//結果セットをViewへ送るための準備
         	while(rs.next()){
         		//結果セットから取得
-        		String id=rs.getString("id");
-        		String password=rs.getString("password");
+        		userData[0]=rs.getString("user_id");
+        		userData[1]=rs.getString("name");
+        		userData[2]=rs.getString("password");
         	}
+        }catch(ClassNotFoundException e) {
+        	e.printStackTrace();
         }
+    			System.out.println("ドライバが見つかりません");
+	    }catch(SQLException e) {
+    		e.printStackTrace();
+    	 }
+    			System.out.println("SQLに関するエラーです");		
+        }
+		//try-with-resourcesによりconnとpstmtは自動的にクローズされる
+		return null;
 	}
 	}
 
-}
+
