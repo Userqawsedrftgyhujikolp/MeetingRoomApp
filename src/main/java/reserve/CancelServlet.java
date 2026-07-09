1c1cc0cf8656885bac8c2bf54d329e9c54941473
takeshita-05 <intecs.2603ap.05@gmail.com>
2026-07-09 15:04:27 +0900

    CancelServlet更新しました

--- src/main/java/reserve/CancelServlet.java
+++ src/main/java/reserve/CancelServlet.java
@@ -8,6 +8,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import bean.ReservationBean;
+
 @WebServlet("/CancelServlet")
 public class CancelServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
@@ -16,6 +18,23 @@ public class CancelServlet extends HttpServlet {
         super();
     }
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//リクエストで受信した文字をUTF-8文字コードで受信する
+		request.setCharacterEncoding("UTF-8");
+	//データ受信
+		String id = request.getParameter("id");
+		String roomId = request.getParameter("roomId");
+		String date = request.getParameter("date");
+		String start = request.getParameter("start");
+		String end = request.getParameter("end");
+		String userId = request.getParameter("userId");
+	//DAOのメソッド呼び出す
+		ReservationBean rList= ReservationBean.delete(); 
+		ReservationBean sakujo = new ReservationBean(id,roomId,date,start,end,userId);
+	//取得したデータをリクエスト属性に格納
+		
+		
+		
+		
 		
 	}
 
